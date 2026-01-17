7e3e5387f92a55739a1e16f098f32fcde41c0527
tanver909 <tanveermahidi@gmial.com>
2026-01-17 22:07:31 +0600

    knowledge edited

--- app/knowledge/page.jsx
+++ app/knowledge/page.jsx
@@ -13,7 +13,7 @@ function page() {
                 <title>Knowledge panel | Mans Packaging</title>
             </Helmet> */}
           <div className='w-10/12 mx-auto mt-5 grid md:grid-cols-5'>
-            <h3 className='md:text-4xl uppercase col-span-2 font-bold'>knowledge panel</h3>
+            <h3 className='md:text-4xl text-black uppercase col-span-2 font-bold'>knowledge panel</h3>
             <p className='col-span-3 text-blue-900 font-medium pt-5 md:pt-0'>Our blog is dedicated to providing valuable insights and information on all things related to printing and packaging. Our team of experts has a wealth of knowledge and experience in the industry, and they are passionate about sharing their insights and expertise with you.</p>
           </div>
           <NewsPage/>
